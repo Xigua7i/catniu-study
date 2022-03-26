3144653a2e8a8ebf3c3bb4e89ece0075a59c21ce
Ben.Niu <KoreyNiu@gmail.com>
2022-03-26 21:13:08 +0800

    Feat add base springboot

---
+++ spring/src/main/java/com/catniu/study/spring/MySpringApplication.java
@@ -0,0 +1,17 @@
+package com.catniu.study.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringApplication
+ *
+ * @author NiuBen KoreyNiu@gmail.com
+ * @date 2022/3/25 22:28
+ */
+@SpringBootApplication
+public class MySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApplication.class);
+    }
+}
---
+++ spring/src/test/java/com/catniu/study/MySpringApplicationTest.java
@@ -0,0 +1,20 @@
+package com.catniu.study;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MySpringApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
